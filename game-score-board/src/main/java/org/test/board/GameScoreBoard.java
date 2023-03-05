81048c6817f98700f91361e5c03d0239a541825a
Ismael <ext-ismael.ortega@decidesoluciones.es>
2023-03-05 13:15:20 +0100

    Add createNewGame Test
    -> Fake solution for now
    + Add Maven's pom
    + Add Junit dependence
    + Add IntelliJ IDEA and Maven files to gitignore
    + Create GameScoreBoard class

---
+++ game-score-board/src/main/java/org/test/board/GameScoreBoard.java
@@ -0,0 +1,13 @@
+package org.test.board;
+
+import java.util.List;
+
+public class GameScoreBoard {
+    public void createGame(String homeTeam, String awayTeam) {
+
+    }
+
+    public List<String> getSummary() {
+        return List.of("Mexico 0 - Canada 0");
+    }
+}
---
+++ game-score-board/src/test/java/org/test/GameScoreBoardTest.java
@@ -0,0 +1,22 @@
+package org.test;
+
+import org.junit.jupiter.api.Test;
+import org.test.board.GameScoreBoard;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
+
+class GameScoreBoardTest {
+
+    @Test
+    void createNewGameOnScoreBoardWithInitialScore() {
+        GameScoreBoard board = new GameScoreBoard();
+        board.createGame("Mexico", "Canada");
+
+        List<String> summary = board.getSummary();
+        List<String> expectedSummary = List.of("Mexico 0 - Canada 0");
+
+        assertLinesMatch(expectedSummary, summary);
+    }
+}
